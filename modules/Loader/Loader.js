de69ced54581f5842e838db4e637c8c4d4202aea
Alexander Lizin <sogimu@nxt.ru>
2013-10-07 10:30:29 +0700

    Added new class Loader, new version 0.5.1

---
+++ modules/Loader/Loader.js
@@ -0,0 +1,164 @@
+/**
+ * Описывает класс Loader.
+ *
+ * @constructor
+ * @param {object} O
+ * @param {string} O.images Содержит объект с именами и путями к некоторым изображениям   
+ * @param {number} O.songs  Содержит объект с именами и путями к некоторой музыке
+ * @this {ArmContext.Loader}
+ * @author <a href="mailto:sogimu@nxt.ru">Alexander Lizin aka Sogimu</a>
+ * @version 0.1
+ */
+
+(function(window) {
+    var Loader = function( O ) {
+
+        gizmo.Filter(O,"Object");
+
+        var me = {};
+
+        me._loadList = [];
+        me._countLoadedObjects = 0;
+        me._isLoadedAllObjects = false;
+        me._typies = ["image","song"];
+
+        me._AddNewObjectToLoadList = function(name, path, type) {
+            this._loadList.push({name: name, path: path, type: type, object: null, isLoaded: false});
+
+        };
+
+        me._onLoadObject = function(object) {
+            this._countLoadedObjects++;
+            if(this.__onLoadObject) {
+                var progress = (this._countLoadedObjects / this._loadList.length) * 100;
+                this.__onLoadObject( object, progress);
+            };
+            if(this._countLoadedObjects >= this._loadList.length) {
+                this._onLoadObjects();
+            }
+        };
+
+        me._onLoadObjects = function() {
+            if(this.__onLoad) {
+                this.__onLoad();
+            };
+            this._isLoadedAllObjects = true;
+
+        };
+
+        me.Start = function(O) {
+            var Hadlers = [];
+
+            var self = this;
+            
+            Hadlers[0] = function(object) {
+                var image = new Image();
+                var hadler = function() {
+                    object.object = image;
+                    object.isLoaded = true;
+                    self._onLoadObject(object);
+                };
+
+                image.onload = hadler;
+            
+                image.src = object.path;
+            };
+
+            Hadlers[1] = function(name, path) {
+                // Has not realisaton
+                console.log("Has not realisaton");
+
+            };
+
+            for(var object in this._loadList) {
+
+                var typies = this._typies;
+                
+                switch( this._loadList[object].type ) {
+                    case typies[0] : {
+                        Hadlers[0]( this._loadList[object] );
+                        
+                    }; break;
+                    
+                    case typies[1] : {
+                        Hadlers[1]( this._loadList[object] );
+                    };
+                    break;
+                };
+            
+            };
+
+            return this;
+
+        };
+
+        me.GetObject = function(name) {
+            gizmo.Filter(name,"String");
+            var haveObjectWithName = false;
+            var objectIndex = -1;
+
+            var haveObject = function(element, index, array) {
+                if(element.name == name) {
+                    haveObjectWithName = true;
+                    objectIndex = index;
+                };
+            };
+            this._loadList.forEach(haveObject);
+
+            if(haveObjectWithName && objectIndex != -1) {
+                if(this._loadList[objectIndex].isLoaded) {
+                    return this._loadList[objectIndex].object;
+
+                } else {
+                    throw Error("Object " + name + " is not loaded yet!");
+
+                };
+
+            } else {
+                throw Error("Object " + name + " is not defined!");
+
+            };
+        };
+
+        me.SetLisener = function(name, func) {
+            gizmo.Filter(name,"String");
+            gizmo.Filter(func,"Function");
+            if(name && func) {
+                this['__' + name] = func;
+            }
+
+            return this;
+
+        };
+
+        me.Set = function(O) {
+            var typies = this._typies;
+            
+            for(var type in O) {
+                switch( type ) {
+                    case typies[0] : {
+                        for (var name in O[ typies[0] ]) {
+                            this._AddNewObjectToLoadList( name, O[ typies[0] ][name], typies[0] );
+                        };
+                    }; break;
+                    
+                    case typies[1] : {
+                        for (var name in O[ typies[1] ]) {
+                            this._AddNewObjectToLoadList( name, O[ typies[1] ][name], typies[1] );
+                        };
+                    };
+                    break;
+                }
+            }
+
+        };
+
+        me.Set( O );
+
+        return me;
+
+    };
+
+    window.ArmContext.Loader = Loader;
+
+})(window);     
--- test/main.js
+++ test/main.js
@@ -1,70 +1,47 @@
 window.onload = function() {
 
     layer1 = ArmContext.Layer({width: 1300, height: 600, name: "layer1", container: "container"});
-    CTX = layer1._ctx;
+    
+    loader = new ArmContext.Loader({
+    image: {
+        "gras": "img/gras.jpg", 
+        "googlebot-kun-1b.png": "img/googlebot-kun-1b.png",
+        "images (1).jpg": "img/images (1).jpg",
+        "item.png": "img/item.png",
+        "nosov_01.jpg": "img/nosov_01.jpg"
+    }})
+    .SetLisener("onLoadObject", function(object, progress) {
+        console.log(object.name + " was loaded!");
+        console.log("Progress: " + progress + "%");
 
-    var Image0 = new Image();
-    Image0.src = "img/gras.jpg";
-    Image0.onload = function() {
-    
-    A = new ArmContext.Rect({layer: layer1, width : 100, height: 100/*, lineDash: [1,2,3]*/, lineWidth: 12, globalAlpha: 1, strokeObject: "#ff0000", fillObject: "#00ff00", /*shadowOffsetX: 15, shadowOffsetY: 15,*/ shadowColor: "#0000ff", zindex: 23})
-    .TranslateTo({x: 200, y: 100})
-    A.name = "A";
-    
-    B = new ArmContext.Rect({layer: layer1, width : 100, height: 100, lineDash: [1,2,3], lineWidth: 63, globalAlpha: 0.5, strokeObject: "#ff0000", fillObject: "#00ff00", /*shadowOffsetX: 15, shadowOffsetY: 15,*/ shadowColor: "#0000ff", zindex: 23})
-    .TranslateTo({x: 250, y: 150})
-    B.name = "B";
-    
-    // C = new ArmContext.Image({layer: layer1, image: Image0, width : 100, height: 100})
-    // .TranslateTo({x: 450, y: 100})
-    // C.name = "C";
-    
-    // D = new ArmContext.Image({layer: layer1, image: Image0, width : 100, height: 100})
-    // .TranslateTo({x: 500, y: 150})
-    // D.name = "D";
+        var img = new ArmContext.Image({layer: layer1, image: this.GetObject(object.name), width : 100, height: 100})
+        .TranslateTo({x: Math.random() * layer1._width, y: 100})
+        img.name = object.name;
+    })
+    .SetLisener("onLoad", function(object) {
+        console.log("All objects where loaded!");
+
+        A = new ArmContext.Rect({layer: layer1, width : 100, height: 100/*, lineDash: [1,2,3]*/, lineWidth: 12, globalAlpha: 1, strokeObject: "#ff0000", fillObject: "#00ff00", /*shadowOffsetX: 15, shadowOffsetY: 15,*/ shadowColor: "#0000ff", zindex: 23})
+        .TranslateTo({x: 200, y: 100})
+        A.name = "A";
         
-    setInterval(function() {
-        A.Rotate({gradAngle: 1, x: 100, y: 100})
-        B.Rotate({gradAngle: -1, x: 100, y: 100})
-        // A._debug.ShowDebugInfo(CTX);
-        // A._globalRepresentation.ShowPoints(CTX);
-        // B._globalRepresentation.ShowPoints(CTX);
+        D = new ArmContext.Image({layer: layer1, image: this.GetObject("googlebot-kun-1b.png"), width : 300, height: 300})
+        .TranslateTo({x: 50, y: 50})
+        D.name = "D";
+            
+        setInterval(function() {
+            A.Rotate({gradAngle: 1, x: 100, y: 100})
+            D.Rotate({gradAngle: -1, x: 100, y: 100})
+            // A._debug.ShowDebugInfo(CTX);
+            // A._globalRepresentation.ShowPoints(CTX);
+            // B._globalRepresentation.ShowPoints(CTX);
 
-        // A._internalRepresentation.ShowPoints(CTX);
-        // B._internalRepresentation.ShowPoints(CTX);
-        layer1.ReDraw();                 
-    }, 10 );
-    
-    // layer1._canvasElement.onmousemove = function(e) {
-    //     // if(A.HasPoint({x: e.offsetX, y: e.offsetY})) {
-    //     //     console.log("Win!");
-
-    //     // }
-    //     lastObj = null;
-    //     var tmpObj = layer1.GetTopestPrimitiveUnderPoint({x: e.offsetX, y: e.offsetY});
-    //     if(tmpObj != lastObj) {
-    //         lastObj = tmpObj;
-    //         console.log(lastObj.name);
-
-    //     }
-    // };
-
-    // layer1.SetLisener("onMouseMove",function(e) {
-    //     // if(A.HasPoint({x: e.offsetX, y: e.offsetY})) {
-    //     //     console.log("Win!");
-
-    //     // }
-    //     lastObj = null;
-    //     var tmpObj = layer1.GetTopestPrimitiveUnderPoint({x: e.offsetX, y: e.offsetY});
-    //     if(tmpObj != lastObj) {
-    //         lastObj = tmpObj;
-    //         console.log(lastObj.name);
-
-    //     }
-    // });
-
-    // layer1.ListenMouseEvents();
-
-}
+            // A._internalRepresentation.ShowPoints(CTX);
+            // B._internalRepresentation.ShowPoints(CTX);
+            layer1.ReDraw();                 
+        }, 10 );
+        
+    })
+    .Start();
 
 };
